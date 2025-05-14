29c1985cc65e0638c9ca7370242aeffaa8577663
Esteban Sánchez <estebanasp8@gmail.com>
2025-05-14 16:45:47 -0500

    Initial commit

--- src/main/java/servlet/CabecerasHttpRequestServlet.java
+++ src/main/java/servlet/CabecerasHttpRequestServlet.java
@@ -13,53 +13,54 @@ import java.util.Enumeration;
 @WebServlet("/cabeceras-request")
 public class CabecerasHttpRequestServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp ) throws IOException, ServletException {
-    resp.setContentType("text/html;charset=UTF-8");
-    String metodoHttp= req.getMethod();
-    String requestUri = req.getRequestURI();
-    String requestUrl= req.getRequestURL().toString();
-    String contextPath = req.getContextPath();
-    String servletPath=req.getServletPath();
-    String ip= req.getLocalAddr();
-    int port=req.getLocalPort();
-    String scheme = req.getScheme();
-    String host = req.getHeader("host");
-    String url=scheme+"://"+ host+":" +contextPath+servletPath;
-    String url2= scheme+"://"+ip+":"+port+contextPath+servletPath;
-    String ipCliente= req.getRemoteAddr();
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF -8");
+        String metodoHttp = req.getMethod();
+        String requestUri = req.getRequestURI();
+        String requesturl = req.getRequestURL().toString();
+        String contextPath = req.getContextPath();
+        String serlvetPath = req.getServletPath();
+        String ip = req.getLocalAddr();
+        int port = req.getLocalPort();
+        String scheme = req.getScheme();
+        String host = req.getHeader("host");
+        String url = scheme + "://" + host + " : " + contextPath + serlvetPath;
+        String url2 = scheme + ":// " + ip + ":" + port + contextPath + serlvetPath;
+        String ipCliente = req.getRemoteAddr();
 
-    try (PrintWriter out = resp.getWriter()) {
-        //Crear la plantilla html
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset=\"utf-8\">");
-        out.println("<title>Cabeceras Http Request</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Cabeceras Http Request</h1>");
-        out.println("<ul>");
-        out.println("<li>Obteniendo el moetodo: "+metodoHttp+"</li>");
-        out.println("<li>Request uri: "+ requestUri+"</li>");
-        out.println("<li>Request url: "+ requestUrl+"</li>");
-        out.println("<li>Context Path: "+ contextPath+"</li>");
-        out.println("<li>Servlet Path: "+ servletPath+"</li>");
-        out.println("<li>IP: "+ ip+"</li>");
-        out.println("<li>Port: " + port+"</li>");
-        out.println("<li>Scheme: "+ scheme+"</li>");
-        out.println("<li>Host: "+ host+"</li>");
-        out.println("<li>URL: "+ url+"</li>");
-        out.println("<li>URL2: "+ url2+"</li>");
-        out.println("<li>Remote Cliente: " + ipCliente+"</li>");
-        Enumeration<String> headerNames=req.getHeaderNames();
-        while(headerNames.hasMoreElements()){
-            String cabecera=headerNames.nextElement();
-            out.println("<li>"+cabecera+": "+req.getHeader(cabecera)+"</li>");
-        }
-        out.println("</ul>");
-        out.println("</body>");
-        out.println("</html>");
-
+        try (PrintWriter out = resp.getWriter()) {
+            //Creo ta plantilla html
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<meta charset=\"UTF-8\">");
+            out.println("<title>Cabeceras Http request</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Cabeceras Http request!</h1>");
+            out. println("<ul>");
+            out.println("<li>Obteniendo el método: "+ metodoHttp +"</li>");
+            out.println("<li> request uri: "+ requestUri +"</li>");
+            out.println("<li> request url: "+ requesturl +"</li>");
+            out.println("<li> Context Path: "+ contextPath +"</li>");
+            out.println("<li> Servlet Path: "+ serlvetPath +"</li>");
+            out.println("<li> IP: "+ ip +"</li>");
+            out.println("<li> Port : "+ port +"</li>");
+            out.println("<li>Scheme: "+ scheme +"</li>");
+            out.println("<li>Host: "+ host +"</li>");
+            out.println("<li>URL: "+ url +"</li>");
+            out.println("<li>URL2: "+ url2 +"</li>");
+            out.println("<li>Remote Cliente: "+ ipCliente +"</li>");
+            Enumeration<String> headerNames = req.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String cabecera = headerNames.nextElement();
+                out.println("<li>" + cabecera+ ": " + req.getHeader(cabecera)+ "</li>");
             }
+            out.println("</ul>");
+            out.println("</body>");
+            out.println("</html>");
+
+
+        }
     }
 }
--- src/main/java/servlet/ProductoXlsServlet.java
+++ src/main/java/servlet/ProductoXlsServlet.java
@@ -1,77 +1,96 @@
 package servlet;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import service.ProdcutoServiceImplement;
+import service.ProductoService;
+import models.Producto;
 
-import javax.sql.rowset.serial.SerialException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import models.Producto;
-import service.ProductoService;
-import service.ProdcutoServiceImplement;
 
-//Implementamos la anotacion
-@WebServlet({"/productos.xls","/productos.json" ,"/prodictos.html"})
-
+@WebServlet({"/productos.xls","/productos.json","/productos.html"})
 public class ProductoXlsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 
         ProductoService service = new ProdcutoServiceImplement();
         List<Producto> productos = service.listar();
-        resp.setContentType("text/html;charset=UTF-8");
+
         String servletPath = req.getServletPath();
         boolean esXls = servletPath.endsWith(".xls");
         boolean esJson = servletPath.endsWith(".json");
-    if (esXls) {
-        resp.setContentType("application/vnd.ms-excel");
-        resp.setHeader("Content-Disposition", "attachment; filename=productos.xls");
-    } else if (esJson) {
-        resp.setContentType("application/json");
-        resp.setHeader("Content-Disposition", "attachment; filename=productos.json");
-    } else {
-        resp.setContentType("text/html");
-    }
-    try (PrintWriter out = resp.getWriter()) {
-        if (!esXls) {
-            //Crear la plantilla html
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<meta charset=\"UTF-8\">");
-            out.println("<title>Listado de Productos</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1> Listado de Productos</h1>");
-            out.println("<p><a href=\""+ req.getContextPath()+ "/productos.xls"+ "\"> Exportar a Excel</a> </p>");
-            out.println("<p><a href=\""+ req.getContextPath() +"/productos.json"+ "\"> Mostar en JSON</a> </p>");
-            out.println("<table>");
-            out.println("<tr>");
-            out.println("<th>ID</th>");
-            out.println("<th>Nombre</th>");
-            out.println("<th>Tipo</th>");
-            out.println("<th>Precio</th>");
-            out.println("</tr>");
-            productos.forEach(p-> {
+
+        if (esXls) {
+            resp.setContentType("application/vnd.ms-excel");
+            resp.setHeader("Content-Disposition", "attachment; filename=productos.xls");
+        } else if (esJson) {
+            resp.setContentType("application/json;charset=UTF-8");
+        } else {
+            resp.setContentType("text/html;charset=UTF-8");
+        }
+
+        try (PrintWriter out = resp.getWriter()) {
+            if (esJson) {
+                // Generar JSON simple
+                out.println("[");
+                for (int i = 0; i < productos.size(); i++) {
+                    Producto p = productos.get(i);
+                    out.println("  {");
+                    out.println("    \"id\": " + p.getId() + ",");
+                    out.println("    \"nombre\": \"" + p.getNombre() + "\",");
+                    out.println("    \"tipo\": \"" + p.getTipo() + "\",");
+                    out.println("    \"precio\": " + p.getPrecio());
+                    out.print("  }");
+                    if (i < productos.size() - 1) {
+                        out.println(",");
+                    } else {
+                        out.println();
+                    }
+                }
+                out.println("]");
+            } else {
+                // Para HTML y XLS generamos tabla HTML
+                if (!esXls) {
+                    out.println("<!DOCTYPE html>");
+                    out.println("<html>");
+                    out.println("<head>");
+                    out.println("<meta charset=\"UTF-8\">");
+                    out.println("<title>Listado de Productos</title>");
+                    out.println("</head>");
+                    out.println("<body>");
+                    out.println("<h1>Listado de Productos</h1>");
+                    out.println("<p><a href=\"" + req.getContextPath() + "/productos.xls\">Exportar a Excel</a></p>");
+                    out.println("<p><a href=\"" + req.getContextPath() + "/productos.json\">Mostrar en JSON</a></p>");
+                }
+
+                out.println("<table border='1'>");
                 out.println("<tr>");
-                out.println("<td>" + p.getId() + "</td>");
-                out.println("<td>" + p.getNombre() + "</td>");
-                out.println("<td>" + p.getTipo() + "</td>");
-                out.println("<td>" + p.getPrecio() + "</td>");
+                out.println("<th>ID</th>");
+                out.println("<th>Nombre</th>");
+                out.println("<th>Tipo</th>");
+                out.println("<th>Precio</th>");
                 out.println("</tr>");
-            });
-            out.println("</table>");
-            if(!esXls) {
-                out.println("</body>");
-                out.println("</html>");
 
-        }
-        }
+                for (Producto p : productos) {
+                    out.println("<tr>");
+                    out.println("<td>" + p.getId() + "</td>");
+                    out.println("<td>" + p.getNombre() + "</td>");
+                    out.println("<td>" + p.getTipo() + "</td>");
+                    out.println("<td>" + p.getPrecio() + "</td>");
+                    out.println("</tr>");
+                }
+                out.println("</table>");
+
+                if (!esXls) {
+                    out.println("</body>");
+                    out.println("</html>");
+                }
+            }
         }
     }
-    }
-
+}
